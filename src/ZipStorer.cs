75053f7a687f45e07de2995070224978067dfefe
susson <49363786+susson@users.noreply.github.com>
2019-09-12 10:31:14 +0800

    修改Encoding.GetEncoding编码为 简体中文 (GB2312)

--- src/ZipStorer.cs
+++ src/ZipStorer.cs
@@ -94,7 +94,7 @@ namespace System.IO.Compression
         // Static CRC32 Table
         private static UInt32[] CrcTable = null;
         // Default filename encoder
-        private static Encoding DefaultEncoding = Encoding.GetEncoding(437);
+        private static Encoding DefaultEncoding = Encoding.GetEncoding(936);
 #endregion
 
 #region Public methods
